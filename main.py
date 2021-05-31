7f4d0eb618c6b4754adc780a32b80b8b33832a54
Mais1212 <64730510+Mais1212@users.noreply.github.com>
2021-05-31 19:29:44 +0300

    Unpacked the function parameters

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import random
 
-from dotenv import load_dotenv
 import requests
+
+from dotenv import load_dotenv
 
 
 def delete_png(comic_title):
@@ -25,9 +26,7 @@ def get_random_comic_url():
     return comix_url
 
 
-def post_comic(vk_access_token, group_id, album_comic, comment):
-    owner_id = album_comic["response"][0]["owner_id"]
-    media_id = album_comic["response"][0]["id"]
+def post_comic(vk_access_token, group_id, comment, owner_id, media_id):
     method_name = "wall.post"
     attachments = f"photo{owner_id}_{media_id}"
     params = {
@@ -44,12 +43,12 @@ def post_comic(vk_access_token, group_id, album_comic, comment):
     response.raise_for_status()
 
 
-def save_comic_in_album(comic_json, group_id, vk_access_token):
+def save_comic_in_album(server, photo, image_hash, group_id, vk_access_token):
     method_name = "photos.saveWallPhoto"
     params = {
-        "server": comic_json["server"],
-        "photo": comic_json["photo"],
-        "hash": comic_json["hash"],
+        "server": server,
+        "photo": photo,
+        "hash": image_hash,
         "group_id": group_id,
         "access_token": vk_access_token,
         "v": "5.131"
@@ -57,9 +56,12 @@ def save_comic_in_album(comic_json, group_id, vk_access_token):
     response = requests.post(
         f"https://api.vk.com/method/{method_name}", params=params
     )
-
     response.raise_for_status()
-    return response
+    json_response = response.json()
+
+    owner_id = json_response["response"][0]["owner_id"]
+    media_id = json_response["response"][0]["id"]
+    return owner_id, media_id
 
 
 def upload_comic_to_server(comic_title, upload_url):
@@ -69,8 +71,13 @@ def upload_comic_to_server(comic_title, upload_url):
         }
         response = requests.post(upload_url, files=files)
     response.raise_for_status()
+    json_response = response.json()
 
-    return response.json()
+    server = json_response["server"]
+    photo = json_response["photo"]
+    image_hash = json_response["hash"]
+
+    return server, photo, image_hash
 
 
 def get_address(vk_access_token, group_id):
@@ -119,10 +126,15 @@ def main():
         url = get_random_comic_url()
         comic_title, comic_comment = download_comic(url)
         upload_url = get_address(vk_access_token, group_id)
-        comic_json = upload_comic_to_server(comic_title, upload_url)
-        album_comic = save_comic_in_album(
-            comic_json, group_id, vk_access_token)
-        # post_comic(vk_access_token, group_id, album_comic.json(), comic_comment)
+
+        server, photo, image_hash = upload_comic_to_server(
+            comic_title, upload_url)
+
+        owner_id, media_id = save_comic_in_album(
+            server, photo, image_hash, group_id, vk_access_token)
+
+        post_comic(vk_access_token, group_id,
+                   comic_comment, owner_id, media_id)
 
     except requests.exceptions.HTTPError as exception:
         print(exception)
